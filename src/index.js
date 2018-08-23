30ab41b0c590289c6ee0fc814e7c2d70a25bab24
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-08-23 22:38:27 +0300

    eth2air service extracted to library

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// functions for deploying airdrop
+import { deployContract } from './deployContract';
+import { approveContract } from './approveContract';
+import { generateLinks } from './generateLinks';
+
+// functions for claiming tokens
+import { getAirdropParams } from './getAirdropParams';
+import { claimTokens } from './claimTokens';
+
+
+// library api
+export default {
+    deployContract,
+    approveContract,
+    generateLinks,
+    getAirdropParams,
+    claimTokens
+}
+
